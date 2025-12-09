86e5a3c67d917a5d62581703f3b33f0137301624
Bryam239u <jaramillobryam870@gmail.com>
2025-12-08 21:29:28 -0600

    ya esta hecha

--- ArbitrajeApp/MainPage.xaml.cs
+++ ArbitrajeApp/MainPage.xaml.cs
@@ -2,23 +2,95 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        int goles1 = 0;
+        int goles2 = 0;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
+        private void GolEq1(object? sender, EventArgs e)
         {
-            count++;
+            if (string.IsNullOrEmpty(txtEq1.Text))
+            {
+                txtEq1.Text = "Equipo Local";
+            }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            goles1 += 1;
+            GolesEq1.Text = goles1.ToString();
         }
+
+        private void NoGol_Eq1(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtEq1.Text))
+            {
+                txtEq1.Text = "Equipo Local";
+            }
+
+            if (goles1 > 0)
+            {
+                goles1 -= 1;
+                GolesEq1.Text = goles1.ToString();
+            }
+        }
+
+        private void GolEq2(object? sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtEq2.Text))
+            {
+                txtEq2.Text = "Equipo Visitante";
+            }
+
+            goles2 += 1;
+            GolesEq2.Text = goles2.ToString();
+        }
+
+        private void NoGol_Eq2(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtEq2.Text))
+            {
+                txtEq2.Text = "Equipo Visitante";
+            }
+
+            if (goles2 > 0)
+            {
+                goles2 -= 1;
+                GolesEq2.Text = goles2.ToString();
+            }
+        }
+
+        private void Limpiar(object sender, EventArgs e)
+        {
+            txtEq1.Text = string.Empty;
+            txtEq2.Text = string.Empty;
+            goles1 = 0;
+            goles2 = 0;
+            GolesEq1.Text = goles1.ToString();
+            GolesEq2.Text = goles2.ToString();
+        }
+
+        private void Final(object? sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtEq1.Text))
+            {
+                txtEq1.Text = "Equipo Local";
+            }
+            if (string.IsNullOrEmpty(txtEq2.Text))
+            {
+                txtEq2.Text = "Equipo Visitante";
+            }
+            string r;
+
+            if (goles1 > goles2)
+                r = "Ganador el equipo " + txtEq1.Text;
+            else if (goles1 < goles2)
+                r = "Ganador el equipo " + txtEq2.Text;
+            else
+                r = "Empate entre " + txtEq1.Text + " y " + txtEq2.Text;
+
+            DisplayAlert("Final del Juego", r, "OK");
+        }
+
     }
 }
