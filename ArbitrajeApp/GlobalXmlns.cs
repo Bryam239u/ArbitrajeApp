e541bdfafb30c268c8f3831b3f166f2ea4ede364
Bryam239u <jaramillobryam870@gmail.com>
2025-12-08 20:34:58 -0600

    Agregar archivos de proyecto.

---
+++ ArbitrajeApp/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ArbitrajeApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "ArbitrajeApp.Pages")]
